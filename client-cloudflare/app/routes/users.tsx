38cc9e59cf2af94a889658d65ba980f01783ce3e
kouta <takeuchi180121@gmail.com>
2024-12-10 01:27:48 +0900

    api/usersをtry-catchで囲った

--- client-cloudflare/app/routes/users.tsx
+++ client-cloudflare/app/routes/users.tsx
@@ -3,11 +3,16 @@ import { json, useLoaderData } from "@remix-run/react";
 
 // `loader` 関数でデータを取得する
 export const loader = async ({ context }: LoaderFunctionArgs) => {
+  try {
   const apiUrl = context.cloudflare.env.API_URL;
   const response = await fetch(`${apiUrl}/api/users`);
   const data = await response.json();
   console.log("loader function running!!");
   return json({data});
+  } catch {
+    console.log("Internal Server Error");
+    return json({ message: "Internal Server Error"}, 500);
+  }
 };
 
 // コンポーネントで useLoaderData を使用
